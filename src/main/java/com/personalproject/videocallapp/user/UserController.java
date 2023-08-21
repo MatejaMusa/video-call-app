492abb08590b94077c23c4af89de58a430972db8
MatejaMusa <matejamusa6@gmail.com>
2023-08-21 15:37:51 +0200

    create user controller

---
+++ src/main/java/com/personalproject/videocallapp/user/UserController.java
@@ -0,0 +1,15 @@
+package com.personalproject.videocallapp.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@Slf4j
+public class UserController {
+}
